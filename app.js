a582224fd92a089474f5ab97b5a89e25ca428272
Michael Cazares <michaelcazares@gmail.com>
2024-06-01 21:57:38 -0700

    completed JS Functions exercise

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-console.log("Hello World!\n==========\n");
-
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("Exercise 1: Print Odds Continued\n");
 function printOdds(count = 10) {
-  for (var i = 1; i < count; i++) {
+  for (let i = 1; i < count; i++) {
     if (i % 2 === 1) {
       console.log(i);
     } else {
@@ -12,11 +9,12 @@ function printOdds(count = 10) {
   }
 }
 
-printOdds(10);
+printOdds(20);
+printOdds();
 
-console.log("BONUS EXERCISE :\n==========\n");
+console.log("Exercise 1 Bonus: Print Odds Continued\n");
 
-function printOdds(count = 10) {
+function printOddsBonus(count = 10) {
   if (count < 0) {
     count = -1 * count;
   }
@@ -29,35 +27,104 @@ function printOdds(count = 10) {
   }
 }
 
-printOdds(-10);
+printOddsBonus(-20);
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
-
-function checkAge(age = 16, userName = "Mike") {
-  let aboveSixteen = `Congrats ${userName}, you can drive!`;
-  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+console.log("Exercise 2: Legal?\n");
+function checkAge(name, age) {
+  let oldEnoughMsg = `Congrats ${name}, you can drive!`;
+  let tooYoungMsg = `Sorry ${name}, but you need to wait until you're 16.`;
   if (age >= 16) {
-    console.log(aboveSixteen);
+    console.log(oldEnoughMsg);
   } else {
-    console.log(belowSixteen);
+    console.log(tooYoungMsg);
   }
 }
 
-checkAge(15, "Sally");
-// checkAge(19, 'Sally')
+checkAge("Ben", 15);
+checkAge("Seth", 16);
+checkAge("Tyson", 19);
 
-// More Practice
-console.log("More Practice:\n==========\n");
-// Assign a function to a variable
-let odds = function (count = 12) {
-  for (var i = 1; i < count; i++) {
-    if (i % 2 === 1) {
-      console.log(i);
-    } else {
-      continue;
-    }
+console.log("Exercise 3: Which Quadrant?\n");
+
+function whichQuadrant(x = 0, y = 0) {
+  if (x === 0 && y === 0) {
+    console.log("This point is at the origin.");
+  } else if (x === 0) {
+    console.log(`This point is on the y-axis.`);
+  } else if (y === 0) {
+    console.log(`This point is on the x-axis.`);
+  } else if (x > 0 && y > 0) {
+    console.log(`This point is in Quadrant 1.`);
+  } else if (x < 0 && y > 0) {
+    console.log(`This point is in Quadrant 2.`);
+  } else if (x < 0 && y < 0) {
+    console.log(`This point is in Quadrant 3.`);
+  } else {
+    console.log(`This point is in Quadrant 4.`);
   }
-};
+}
 
-odds(10);
+whichQuadrant(0, 0);
+whichQuadrant(0, 1);
+whichQuadrant(1, 0);
+whichQuadrant(1, 1);
+whichQuadrant(-1, 1);
+whichQuadrant(-1, -1);
+whichQuadrant(1, -1);
+
+console.log("Exercise 4: What type of triangle?\n");
+
+function triangleType(side1 = 0, side2 = 0, side3 = 0) {
+  if (
+    side1 + side2 <= side3 ||
+    side1 + side3 <= side2 ||
+    side2 + side3 <= side1
+  ) {
+    console.log("This is an invalid triangle.");
+  } else if (side1 === side2 && side1 === side3) {
+    console.log("This is an equilateral triangle.");
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log("This is an isosceles triangle.");
+  } else {
+    console.log("This is a scalene triangle.");
+  }
+}
+
+triangleType(1, 1, 2); // invalid
+triangleType(1, 1, 1); // equilateral
+triangleType(1, 2, 2); // isosceles
+triangleType(2, 3, 4); // scalene
+
+console.log("BONUS Exercise 5: Data Plan Status\n");
+
+function dataPlanStatus(planLimit = 100, day = 15, usage = 56) {
+  let daysRemaining = 30 - day;
+  let averageDailyUse = (usage / day).toFixed(2);
+  let averageDailyUseRemaining = ((planLimit - usage) / daysRemaining).toFixed(
+    2
+  );
+  let excessUsage = (usage - planLimit).toFixed(2);
+  let excessDailyUse = (averageDailyUse - averageDailyUseRemaining).toFixed(2);
+
+  console.log(
+    `${day} days used, ${daysRemaining} days remaining\nAverage daily use: ${averageDailyUse} GB/day`
+  );
+
+  if (usage > planLimit) {
+    console.log(
+      `You are EXCEEDING your average daily use (${averageDailyUse} GB/day). Continuing this high usage, you'll exceed your data plan by ${excessUsage} GB.\nTo stay below your data plan, use no more than ${averageDailyUseRemaining} GB/day.`
+    );
+  } else if (usage < planLimit) {
+    console.log(
+      `You are under your average daily use (${averageDailyUse} GB/day). You can afford to use ${averageDailyUseRemaining} GB/day for the rest of the month.`
+    );
+  } else {
+    console.log(
+      `You are right on your average daily use (${averageDailyUse} GB/day). You can afford to use ${averageDailyUseRemaining} GB/day for the rest of the month.`
+    );
+  }
+}
+
+dataPlanStatus(100, 15, 56); // under usage
+dataPlanStatus(100, 15, 100); // right on usage
+dataPlanStatus(100, 15, 101); // over usage
